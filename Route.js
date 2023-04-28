5be7ca533bf1f362ba2ff94f9ebecae9465bfe2b
Katawaytee <ktriampo@gmail.com>
2023-04-28 14:32:23 +0700

    Implement Route.js

---
+++ Route.js
@@ -0,0 +1,14 @@
+// using express.js frameworkk
+const express = require("express");
+
+const Controller = require("../Controller");
+
+const router = express.Router();
+
+// define endpoint for requests
+
+
+
+// -----------------------------
+
+module.exports = router;
