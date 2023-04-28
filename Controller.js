e1d2e079bf54b21a01f1e35087b8994b2982184d
Katawaytee <ktriampo@gmail.com>
2023-04-28 15:53:50 +0700

    Minor change

--- Controller.js
+++ Controller.js
@@ -16,60 +16,61 @@ exports.authApp = (req, res) => {
 exports.accessToken = (req, res) => {
     const parsedUrl = url.parse(req.url);
     const parsedQuery = querystring.parse(parsedUrl.query);
-  
+
     if (parsedQuery.error) {
-      res.writeHead(400, { "Content-Type": "text/plain" });
-      res.end(`Authorization error: ${parsedQuery.error_description}`);
-      return;
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        res.end(`Authorization error: ${parsedQuery.error_description}`);
+        return;
     }
-  
+
     if (parsedQuery.code) {
-      const postData = querystring.stringify({
+        const postData = querystring.stringify({
         grant_type: "authorization_code",
         code: parsedQuery.code,
         client_id: process.env.client_id,
         client_secret: process.env.client_secret,
         redirect_uri: redirect_uri,
-      });
-  
-      const tokenOptions = {
+    });
+
+    const tokenOptions = {
         method: "POST",
         headers: {
-          "Content-Type": "application/x-www-form-urlencoded",
-          "Content-Length": postData.length,
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Content-Length": postData.length,
         },
-      };
-  
-      const tokenReq = https.request(
+    };
+
+    const tokenReq = https.request(
         access_token_url,
         tokenOptions,
         (tokenRes) => {
-          let tokenData = "";
-          tokenRes.on("data", (chunk) => {
-            tokenData += chunk;
-          });
-          tokenRes.on("end", () => {
-            const token = JSON.parse(tokenData);
-            req.session.token = token;
-            console.log(req.session);
-            /*if (token) {
-              res.writeHead(302, {
-                Location: `http://${process.env.frontendIPAddress}/home.html`,
-              });
-              res.end();
-            }*/
-          });
+            let tokenData = "";
+            tokenRes.on("data", (chunk) => {
+                tokenData += chunk;
+            });
+            tokenRes.on("end", () => {
+                const token = JSON.parse(tokenData);
+                req.session.token = token;
+                console.log(req.session);
+                /*if (token) {
+                    res.writeHead(302, {
+                    Location: `http://${process.env.frontendIPAddress}/home.html`,
+                    });
+                    res.end();
+                }*/
+            });
         }
-      );
-      tokenReq.on("error", (err) => {
+    );
+
+    tokenReq.on("error", (err) => {
         console.error(err);
-      });
-      tokenReq.write(postData);
-      tokenReq.end();
-    } else {
-      res.writeHead(302, { Location: authorization_url });
-      res.end();
-    }
-  };
+    });
+    tokenReq.write(postData);
+    tokenReq.end();
+} else {
+    res.writeHead(302, { Location: authorization_url });
+    res.end();
+}
+};
 
 // IMPLEMENT OTHER FUNCTIONS BELOW
